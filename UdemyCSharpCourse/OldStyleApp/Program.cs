a952c64674bb04fcba7891353c1845faa254ea08
Steven Brady <bradys@my.lanecc.edu>
2023-06-19 09:28:13 -0700

    Hello World!

---
+++ UdemyCSharpCourse/OldStyleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OldStyleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine("Was that a beep!!!!!");
+            Console.ReadLine();
+            Console.Beep();
+            Console.Beep();
+            Console.ReadLine();
+        }
+    }
+}
