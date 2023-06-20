8b07c344e2aec61d188257cff3b14a6a365ab152
Steven Brady <bradys@my.lanecc.edu>
2023-06-19 20:02:23 -0700

    finished strings exercise and added folder for python class

---
+++ UdemyCSharpCourse/Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Steven";
+
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+            string lowerMessage = message.ToLower();
+
+            Console.WriteLine(message);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lowerMessage);
+        }
+    }
+}
