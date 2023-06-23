57d596c80388d7346778e9f2ade92402bd8c371a
Steven Brady <bradys@my.lanecc.edu>
2023-06-22 22:39:06 -0700

    started methods section

---
+++ UdemyCSharpCourse/Constants/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Constants
+{
+    // Constants are immutable values which are known
+    // at compile time and do not change for the life of the program
+
+    internal class Program
+    {
+        // constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        // create a constant of type string with your birthday as its value
+        const string birthday = "07.06.1983";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My birthday is always going to be {0}", birthday );
+        }
+    }
+}
---
+++ UdemyCSharpCourse/Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am call from a method.");
+            Console.ReadLine();
+        }
+    }
+}
