5d4ea935f9ef5ef261f21d171c966d2796c20516
Steven Brady <bradys@my.lanecc.edu>
2023-06-24 06:39:41 -0700

    started section 4

---
+++ UdemyCSharpCourse/Section4MakingDecisions/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Section4MakingDecisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the tempurature in degrees F: ");
+            string tempurature = Console.ReadLine();
+            int temp = int.Parse(tempurature);
+
+            Tempurature(temp);
+        }
+
+        static void Tempurature(int temp)
+        {
+            
+            if (temp < 50)
+            {
+                Console.WriteLine("Take a coat");
+            }
+
+            if (temp ==  50)
+            {
+                Console.WriteLine("It's 50 degrees F");
+            }
+
+            if (temp > 50)
+            {
+                Console.WriteLine("It's cozy & warm");
+            }
+        }
+    }
+}
