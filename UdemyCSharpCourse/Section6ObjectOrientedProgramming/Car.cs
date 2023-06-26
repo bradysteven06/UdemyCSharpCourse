88fc26de83047f7ed228a337cde6668dd6dceccd
Steven Brady <bradys@my.lanecc.edu>
2023-06-26 09:21:27 -0700

    started section 6 - Object Oriented Programming

---
+++ UdemyCSharpCourse/Section6ObjectOrientedProgramming/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section6ObjectOrientedProgramming
+{
+    internal class Car
+    {
+        // Constructor
+        public Car() 
+        {
+            Console.WriteLine("Car was created!");
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car stopped!");
+        }
+    }
+}
---
+++ UdemyCSharpCourse/Section6ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Section6ObjectOrientedProgramming
+{
+    // A class in C# (and most other object-oriented programminglanguages)
+    // is a blueprint for creating objects.
+    // It defines a wet of properties (data attributes)
+    // and methods (functions) that an object of that class will have.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car();
+            audi.Drive();
+            Car bmw = new Car();
+
+            Console.WriteLine("Press 1 to stop the car!");
+            string userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                audi.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Car keeps driving");
+            }
+        }
+    }
+}
