b78ec1840275f224e96662fe490db326cf06d744
Steven Brady <bradys@my.lanecc.edu>
2023-06-25 20:24:11 -0700

    finished do while loops

--- UdemyCSharpCourse/Section5Loops/Program.cs
+++ UdemyCSharpCourse/Section5Loops/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ForLoops();
+        }
+
+        static void ForLoops()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static void DoWhileLoops()
+        {
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.Write("Please enter the name of a friend: ");
+                string nameOfFriend = Console.ReadLine();
+                int currentLength = nameOfFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfFriend;
+            } while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough! " + wholeText);
+        }
+
+        static void  WhileLoops()
+        {
+
+        }
+
+        static void BreakContinue()
+        {
+
         }
     }
 }
