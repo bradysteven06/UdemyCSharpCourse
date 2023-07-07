09095ff439a0466f7604d673931b117bf6aa2928
Steven Brady <bradys@my.lanecc.edu>
2023-07-06 22:28:44 -0700

    finished arrays as parameters lecture

---
+++ UdemyCSharpCourse/ArraysAsParameters/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ArraysAsParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 16 };
+            double averageResult = GetAverage(studentsGrades);
+
+            int[] happiness = { 2, 3, 4, 5, 6 };
+            SunIsShining(happiness);
+
+            foreach(int y in happiness)
+            {
+                Console.WriteLine(y);
+            }
+
+            foreach(int grade in studentsGrades)
+            {
+                Console.WriteLine(" {0} ", grade);
+            }
+
+            Console.WriteLine("The average is: {0}", averageResult);
+        }
+
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+
+            for(int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+            average = (double)sum / size;
+            return average;
+        }
+
+        static void SunIsShining(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] += 2;
+            }
+        }
+    }
+}
