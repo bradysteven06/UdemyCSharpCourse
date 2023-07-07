2bbc8fb3d3c066cebee42c082849ddf049fa63f5
Steven Brady <bradys@my.lanecc.edu>
2023-07-07 05:21:18 -0700

    finised Hashtable coding exercise

--- UdemyCSharpCourse/Hashtables/Program.cs
+++ UdemyCSharpCourse/Hashtables/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace Hashtables
+﻿using System;
+using System.Collections;
+
+namespace Hashtables
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hashtable studentsTable = new Hashtable();
+
+            Student stud1 = new Student(1, "Maria", 98);
+            Student stud2 = new Student(2, "Jason", 76);
+            Student stud3 = new Student(3, "Clara", 43);
+            Student stud4 = new Student(4, "Steve", 55);
+
+            studentsTable.Add(stud1.Id, stud1);
+            studentsTable.Add(stud2.Id, stud2);
+            studentsTable.Add(stud3.Id, stud3);
+            studentsTable.Add(stud4.Id, stud4);
+
+            // retrieve individual item with known ID
+            Student storedStudent1 = (Student)studentsTable[stud1.Id];
+
+            // retrieve all values from a Hashtable
+            foreach (DictionaryEntry entry in studentsTable)
+            {
+                Student temp = (Student)entry.Value;
+                Console.WriteLine("Student ID: {0}", temp.Id);
+                Console.WriteLine("Student Name: {0}", temp.Name);
+                Console.WriteLine("Student GPA: {0}", temp.GPA);
+            }
+
+            foreach (Student value in studentsTable.Values)
+            {
+                Console.WriteLine("Student ID: {0}", value.Id);
+                Console.WriteLine("Student Name: {0}", value.Name);
+                Console.WriteLine("Student GPA: {0}", value.GPA);
+            }
+
+            Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA);
+
+        }
+    }
+
+    class Student
+    {
+        //property called Id
+        public int Id { get; set; }
+        //property called Name
+        public string Name { get; set; }
+        //property called GPA
+        public float GPA { get; set; }
+        //simple constructor
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
         }
     }
 }
---
+++ UdemyCSharpCourse/HashtablesChallenge/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace HashtablesChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student(1, "Denis", 88);
+            students[1] = new Student(2, "Olaf", 97);
+            students[2] = new Student(6, "Ragner", 65);
+            students[3] = new Student(1, "Luise", 73);
+            students[4] = new Student(4, "Levi", 58);
+
+            Hashtable studentsTable = new Hashtable();
+
+            foreach (Student student in students)
+            {
+                if (studentsTable.ContainsKey(student.Id))
+                {
+                    Console.WriteLine("Sorry, a student with the same ID already exists. ID: {0}", student.Id);
+                }
+                else
+                {
+                    studentsTable.Add(student.Id, student);
+                }
+            }
+
+            foreach (DictionaryEntry entry in studentsTable)
+            {
+                Student temp = (Student)entry.Value;
+                Console.WriteLine("Student ID: {0}", temp.Id);
+                Console.WriteLine("Student Name: {0}", temp.Name);
+                Console.WriteLine("Student GPA: {0}", temp.GPA);
+            }
+        }
+    }
+
+    class Student
+    {
+        //property called Id
+        public int Id { get; set; }
+        //property called Name
+        public string Name { get; set; }
+        //property called GPA
+        public float GPA { get; set; }
+        //simple constructor
+        public Student(int id, string name, float GPA)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.GPA = GPA;
+        }
+    }
+}
