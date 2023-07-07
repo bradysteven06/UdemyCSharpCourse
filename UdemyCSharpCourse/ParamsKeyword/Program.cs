4487b1d9e1296eb3d55f6d97d6305b208516051e
Steven Brady <bradys@my.lanecc.edu>
2023-07-07 03:57:11 -0700

    finised params keyword lecture

---
+++ UdemyCSharpCourse/ParamsKeyword/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ParamsKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int min = MinV2(6, 4, 2, 8, -20, 1, 5);
+
+            Console.WriteLine("The minimum is: {0}", min);
+        }
+
+        public static int MinV2(params int[] numbers) 
+        {
+            int min = int.MaxValue;
+
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
+        }
+    }
+}
