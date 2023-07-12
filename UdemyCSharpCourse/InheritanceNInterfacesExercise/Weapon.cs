c30c0e09a101b7ae65c036877dd081b8b857fedb
Steven Brady <bradys@my.lanecc.edu>
2023-07-12 03:26:26 -0700

    finished inheritance and interfaces coding exercise

---
+++ UdemyCSharpCourse/InheritanceNInterfacesExercise/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceNInterfacesExercise
+{
+    class Weapon
+    {
+        public string Name { get; set; }
+
+        public Weapon()
+        {
+            Name = "Weapon";
+        }
+
+        public Weapon(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Label()
+        {
+            Console.WriteLine("This is {0}!", Name);
+        }
+    }
+}
