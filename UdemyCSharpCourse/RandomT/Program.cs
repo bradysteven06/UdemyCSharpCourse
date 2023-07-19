58c7dcf318fc4a7a1663aa3e2e745bdd2870d5ae
Steven Brady <bradys@my.lanecc.edu>
2023-07-19 04:21:08 -0700

    finished Random Class lecture

---
+++ UdemyCSharpCourse/RandomT/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+
+namespace RandomT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int numEyes;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numEyes = dice.Next(1, 7);
+                Console.WriteLine(numEyes);
+            }
+
+            FortuneTeller();
+
+            Console.ReadLine();
+        }
+
+        static void FortuneTeller()
+        {
+            Random num = new Random();
+            int randomNum = num.Next(1, 3);
+
+            Console.WriteLine("Ask a question get an answer.");
+            Console.Write("What is your question? ");
+            Console.ReadLine();
+
+            if (randomNum == 1)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (randomNum == 2)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Maybe");
+            }
+        }
+    }
+}
