218453db117a0e8b63327bd52dc9b6d5e71a90f3
Steven Brady <bradys@my.lanecc.edu>
2023-07-19 05:21:46 -0700

    finished Parsing Game part 2

--- UdemyCSharpCourse/ParsingGame2/Program.cs
+++ UdemyCSharpCourse/ParsingGame2/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ParsingGame2
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ParsingGame2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Receive the Text line by line.
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Courses\UdemyC#Course\UdemyCSharpCourse\UdemyCSharpCourse\ParsingGame2\assets\input2.txt");
+            string pattern = @"\b\d{2,3}\b";
+
+            Regex regex = new Regex(pattern);
+
+            foreach (string line in lines)
+            {
+                MatchCollection matchingIntegers = regex.Matches(line);
+                foreach (Match integer in matchingIntegers)
+                {
+                    int num = int.Parse(integer.Value);
+                    Console.WriteLine((char)num);
+                }
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
